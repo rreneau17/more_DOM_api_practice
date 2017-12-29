af9cbbbf6d4dd33f7dfa38cf077cbb13e4c4d562
Rich Reneau <rich.reneau@gmail.com>
2017-12-28 20:17:34 -0500

    added JSON parse function and resized thumbnails to 400x400

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,32 @@
 // pulls movie data via AJAX method
 function getData() {
-    // var url = `https://my-little-cors-proxy.herokuapp.com/https://itunes.apple.com/search?term=sideways&entity=movie&limit=10`;
-    var url = `https://my-little-cors-proxy.herokuapp.com/http://api.walmartlabs.com/v1/search?query=sideways&format=json&apiKey=cwkwmzcnca9e8jtcw34wpppc&facet=on&categoryId=4096`;
+    var url = `https://my-little-cors-proxy.herokuapp.com/https://itunes.apple.com/search?term=sideways&entity=movie&limit=10`;
+    // var url = `https://my-little-cors-proxy.herokuapp.com/http://api.walmartlabs.com/v1/search?query=sideways&format=json&apiKey=cwkwmzcnca9e8jtcw34wpppc&facet=on&categoryId=4096`;
     return $.get(url);
+}
+
+// parses JSON string  
+function parseData(movies) {
+    return JSON.parse(movies);
+}
+
+// resizes thumbnail pic to 400x400 pixels
+function resizePics(data) {
+    data.results.forEach( function(result) {
+        result.largerPic = result.artworkUrl100.replace("100x100","400x400");
+    });
+    return data;
 }
 
 function logData(movies) {
     console.log(movies);
 }
 
+
 function main() {
     getData()
+    .then (parseData)
+    .then (resizePics)
     .then (logData) 
 }
 
